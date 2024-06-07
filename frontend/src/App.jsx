667fcb1223943883b094eedfe2115108cd421d71
zkac334 <maliha.ahmed.2021@live.rhul.ac.uk>
2024-06-07 05:07:20 +0100

    added an about page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from './Pages/Home';
+import About from './Pages/About'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}/>
       </Routes>
     </Router>
   )
---
+++ frontend/src/Pages/About.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function About(){
+
+    return(
+        <h1>
+            About Page
+        </h1>
+    )
+}
+
+export default About;
