8b5d91746bfdff111a373777f48e5b3f07a269a3
zkac334 <maliha.ahmed.2021@live.rhul.ac.uk>
2024-06-08 12:08:02 +0100

    created the css for home page

--- frontend/src/Components/Navigation.jsx
+++ frontend/src/Components/Navigation.jsx
@@ -1,18 +1,23 @@
-import react from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Home from '../Pages/Home';
 import About from '../Pages/About';
 import Contact from '../Pages/Contact';
 import Services from '../Pages/Services';
+import '../App.css';
+import Logo from'../Images/LOGO.png'
 
 function Navigation () {
     
    return (
-    <div class="navigation">
-        <li> <Link to= '/'> Home</Link></li>
-        <li> <Link to= '/about'> About</Link></li>
-        <li> <Link to= '/contact'> Contact</Link></li>
-        <li> <Link to= '/services'> Services</Link></li>
+    <div class="Navbar"> 
+        <img src={Logo} className="Logo" alt="Logo" />
+        <div class="navigation">
+            <li><Link to= '/'> <button>Home</button></Link></li>
+            <li><Link to= '/about'> <button> About</button></Link></li>
+            <li><Link to= '/contact'> <button>Contact</button></Link></li>
+            <li><Link to= '/services'><button> Services</button></Link></li>
+        </div>
     </div>
     );
 }
